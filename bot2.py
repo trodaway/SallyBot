075801d20cc18bf0dc72b291d537abd2fe8256b4
Tim Rodaway <t.rodaway1@ncl.ac.uk>
2020-04-01 21:15:28 +0100

    stopped translator trying to translate commands

--- bot2.py
+++ bot2.py
@@ -269,9 +269,9 @@ async def on_message(message):
 
         # translates every 'x' to geordie
         elif translator_frequency != 0:  # set it to 0 to stop it from translating
-            print(re.match("^<@!?693216082567233667>.*$", message.content))
+            print(re.match("^<@(&|!)?693216082567233667>.*$", message.content))
             if random.randrange(translator_frequency) == 0 and \
-                    re.match("^<@!?693216082567233667>.*$", message.content) is None:
+                    re.match("^<@(&|!)?693216082567233667>.*$", message.content) is None:
                 translated_text = translator(message.content)
                 if translated_text is not None:
                     await channel.send(f"In the Toon we'd say that like:\n>>> {translated_text}")
